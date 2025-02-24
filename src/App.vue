bc4890f90b6ed12f58cbb446f5c60b9073d24f64
laurelcrelia <laurel.matt@helsinki.fi>
2025-02-24 15:43:05 +0200

    Set up Supabase

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import { supabase } from "./lib/supabase";
+
+const message = ref("Connecting...");
+
+onMounted(async () => {
+  let { data: test, error } = await supabase
+    .from('test')
+    .select('*')
+          
+  if (error) {
+    message.value = "Error connecting to Supabase";
+  } else {
+    message.value = `Connected to Supabase. Found ${test.length} rows in 'test' table`;
+  }
+});
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ message }}</h1>
 </template>
 
-<style>
-h1 {
-  text-align: left;
-  padding-left: 2rem;
-  font-size: 2rem;
-  margin-top: 10vh;
-}
-</style>
